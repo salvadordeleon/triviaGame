be796051e4d00afb62c75232ec27cb3e98193b57
Salvador DeLeon <salvadorvdeleon@gmail.com>
2019-09-16 17:10:17 -0600

    updated jquery, added initial value logic.

---
+++ assets/javascript/triviaQuestion.js
@@ -0,0 +1,32 @@
+var triviaQuestion = [
+    {
+        question: " What is the most purchased commuter car?",
+        choices: ["Hyundai Ionic", "Lexus ES350 Hybrid", "Lexus Ct200 Hybrid", "Honda Accord Hybrid"],
+        correctAnswer: "Honda Accord Hybrid"
+    },
+
+    {
+        question: " In 2019 what brand is considered the most reliable?",
+        choices: ["Mercedes Benz", "Toyota", "Honda", "Hyundai"],
+        correctAnswer: "Hyundai"
+    },
+
+    {
+        question: "Which commuter car has the best MPG?",
+        choices: ["Chevrolet Cruz", "Honda Civic", "Hyundai Ioniq", "Ford Escape"],
+        correctAnswer: "Hyundai Ioniq"
+    },
+
+    {
+        question: "What is considered the most practicle commuter car?",
+        choices: ["Toyota Corolla", "Ford Fiesta", "Subaru Impreza", "Kia Forte"],
+        correctAnswer: "Toyota Corolla"
+    },
+
+    {
+        question: "Which commuter car is considered the DEADLIEST???",
+        choices: ["Nissan Sentra", "Subaru legacy", "Toyota Camry", "Ford Fiesta"],
+        correctAnswer: "Nissan Sentra"
+
+    }
+];
